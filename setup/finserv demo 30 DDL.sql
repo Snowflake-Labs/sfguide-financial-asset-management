bf83bd862b6c96effc8423aa2ddc111584274889
Allen Wong <53224894+allen-wong-tech@users.noreply.github.com>
2020-10-30 14:32:31 -0400

    Added big data stress testing on trades
    search for "RAISE THE LIMIT" to see 2 places you can control how many traders and trades are made
    removed DailyTrader and added randomly generated traders and buying_power
    currently set to 100 traders and 1000 stocks so 100 * 1000 = 100K trades per day

--- setup/finserv demo 30 DDL.sql
+++ setup/finserv demo 30 DDL.sql
@@ -4,7 +4,9 @@ Run these scripts to setup DDL
 */
 
 use role finserv_admin; use warehouse finserv_devops_wh; use schema finserv.public;
-alter warehouse finserv_devops_wh set warehouse_size = 'medium';
+
+--size up since we are generating many trades 
+alter warehouse finserv_devops_wh set warehouse_size = 'xlarge';
 
 
 
@@ -29,8 +31,8 @@ alter warehouse finserv_devops_wh set warehouse_size = 'medium';
         select symbol, date, open, high, low, close, volume, adjclose
         from cte
         where num = 1;
-        
-//        select top 10 * from finserv.public.stock_history;
+
+        //        select top 10 * from finserv.public.stock_history;
 
       -----------------------------------------------------
         create or replace view finserv.public.stock_latest
@@ -47,7 +49,7 @@ alter warehouse finserv_devops_wh set warehouse_size = 'medium';
         from cte
         where num = 1;
 
-//        select top 10 * from finserv.public.stock_latest where symbol = 'SBUX';
+        //        select top 10 * from finserv.public.stock_latest where symbol = 'SBUX';
 
       -----------------------------------------------------
         create or replace view finserv.public.company_profile
@@ -56,8 +58,68 @@ alter warehouse finserv_devops_wh set warehouse_size = 'medium';
         select symbol, exchange, companyname, industry, website, description, ceo, sector, beta, mktcap::number mktcap
         from zepl_us_stocks_daily.public.company_profile;
 
-//        select top 300 * from finserv.public.company_profile;
+        //        select top 300 * from finserv.public.company_profile;
 
+
+
+
+
+----------------------------------------------------------------------------------------------------------
+--Control the number of rows generated in trade: 
+      //Create traders for big data volume stress testing 
+      create or replace temp table middleware.temp_trader as
+      select distinct *
+      from
+      (
+          select
+              upper(randstr(3, random()))::varchar(50) trader,
+              upper(randstr(2, random()))::varchar(50) PM,
+              uniform(4000, 8000, random()) buying_power
+          from table (generator(rowcount => 60000))
+      ) c
+      where rlike(trader,'[A-Z][A-Z][A-Z]') = 'TRUE' and rlike(PM,'[A-Z][A-Z]') = 'TRUE';
+
+      --with dupes removed
+      create or replace transient table finserv.public.trader comment = 'Trader with their PM and authorized buying power'
+      as
+      with cte as
+      (
+          select *,
+          row_number() over (partition by trader order by buying_power, PM) num
+          from middleware.temp_trader
+      )
+      select trader, PM, buying_power
+      from cte
+      where num = 1
+      order by 2,1
+      //1 of 2 RAISE THE LIMIT FOR MORE STRESS TESTING
+      //      limit 10000;
+      //      limit 5000;
+            limit 100;
+      
+      comment on column public.trader.PM is 'Portfolio Manager';
+      comment on column public.trader.buying_power is 'Trader is authorized this buying power in each transaction';
+      
+
+
+        --remove authorization of trades that have a close <1 or >4500
+        create or replace temp table middleware.temp_watchlist as
+            select c.*, 'all_authorized' Trader
+            from finserv.public.company_profile c
+            inner join finserv.public.stock_latest l on c.symbol = l.symbol     --ensure stock still traded 
+            where mktcap is not null
+            and exchange like 'N%'
+            and c.symbol not in 
+            (
+                select distinct symbol
+                from finserv.public.stock_history
+                where close < 1 or close > 4500
+            )
+            order by mktcap desc
+        //2 of 2 RAISE THE LIMIT FOR MORE STRESS TESTING
+            //            limit 2000;
+            limit 1000;
+            //            limit 1;  //use for testing
 
 
 
@@ -70,55 +132,93 @@ alter warehouse finserv_devops_wh set warehouse_size = 'medium';
 ----------------------------------------------------------------------------------------------------------
 --Asset Management Firm Objects
 
+
         -----------------------------------------------------
             create or replace table finserv.public.watchlist
                 comment = 'what assets we are interested in owning'
             as
-            select *
+            select *, 'charles'::varchar(50) Trader
             from finserv.public.company_profile
             where symbol in ('AMZN','CAT','COF','GE','GOOG','MCK','MSFT','NFLX','SBUX','VOO','XOM')
-            order by symbol, exchange;
+                union all
+            select * from middleware.temp_watchlist
+            order by trader, symbol, exchange;
             
-//            select top 300 * from watchlist;
+//            select * from watchlist;
+
+
+
+
+
 
         -----------------------------------------------------
         create or replace transient table finserv.public.trade
             comment = 'trades made and cash used; unique_key: symbol, exchange, date'
         as
-        --for DailyTrader buy $100K in value per day when year <> 2019
+        --buy for all traders except Charles
+         select
+              c.*,
+              round(buying_power/close,0) num_shares, 
+              close * round(buying_power/close,0) * -1 cash,
+              t.trader, t.PM
+         from
+         (
             select
-                date, h.symbol, w.exchange, 'buy'::varchar(25) action, close, round(100000/close,0) num_shares, close * round(100000/close,0) * -1 cash,
-                'DailyTrader'::varchar(50) Trader, 'DailyTraderPM'::varchar(50) PM
+                date, h.symbol, w.exchange, 'buy'::varchar(25) action, close
             from finserv.public.stock_history h
-            inner join finserv.public.company_profile w on h.symbol = w.symbol
-            where h.close <> 0 and year(date) <> 2019
-        --for DailyTrader sell $5K in value per day when year = 2019
-        union all
+            inner join finserv.public.watchlist w on h.symbol = w.symbol and w.trader = 'all_authorized'
+            where h.close <> 0 and year(date) between 2010 and 2019
+         ) c
+         full outer join public.trader t
+       union all
+        --hold for all traders except Charles
+         select
+              c.*,
+              0 num_shares, 
+              0 cash,
+              t.trader, t.PM
+         from
+         (
             select
-                date, h.symbol, w.exchange, 'sell' action, close, round(50000/close,0) * -1 num_shares, close * round(50000/close,0) cash,
-                'DailyTrader' Trader, 'DailyTraderPM' PM
+                date, h.symbol, w.exchange, 'hold'::varchar(25) action, close
             from finserv.public.stock_history h
-            inner join finserv.public.company_profile w on h.symbol = w.symbol 
-            where h.close <> 0 and year(date) = 2019
-        union all
+            inner join finserv.public.watchlist w on h.symbol = w.symbol and w.trader = 'all_authorized'
+            where h.close <> 0 and year(date) >= 2020
+         ) c
+         full outer join public.trader t
+       union all
           --for charles buy $100K in value for each ticker in Jan 2019
           select
-              date, h.symbol, w.exchange, 'buy'::varchar(25) action, close, round(1000000/close,0) num_shares, close * round(1000000/close,0) * -1 cash,
+              date, h.symbol, w.exchange, 'buy'::varchar(25) action, close, round(1000000/close,0) num_shares, 
+              close * round(1000000/close,0) * -1 cash,
               'charles' Trader, 'warren' PM
           from finserv.public.stock_history h
-          inner join finserv.public.watchlist w on h.symbol = w.symbol 
+          inner join finserv.public.watchlist w on h.symbol = w.symbol and w.trader = 'charles'
           where h.close <> 0 and year(date) = 2019 and month(date) = 1
+        union all
+          --for charles sell $10K in value for each ticker in Mar 2019
+            select
+                date, h.symbol, w.exchange, 'sell' action, close, round(10000/close,0) * -1 num_shares, 
+                close * round(10000/close,0) cash,
+                'charles' Trader, 'warren' PM
+            from finserv.public.stock_history h
+            inner join finserv.public.watchlist w on h.symbol = w.symbol and w.trader = 'charles'
+            where h.close <> 0 and year(date) = 2019 and month(date) = 3
         union all
           --for charles hold action so shares and cash don't change
           select
               date, h.symbol, w.exchange, 'hold' action, close, 0, 0 cash,
               'charles' Trader, 'warren' PM
           from finserv.public.stock_history h
-          inner join finserv.public.watchlist w on h.symbol = w.symbol 
-          where (h.close <> 0 and year(date) = 2019 and month(date) <> 1) or (h.close <> 0 and year(date) = 2020)
-          order by 8,2,1;
+          inner join finserv.public.watchlist w on h.symbol = w.symbol and w.trader = 'charles'
+          where (h.close <> 0 and year(date) = 2019 and month(date) not in (1,3)) or (h.close <> 0 and year(date) >= 2020)
+        order by 8,2,1;--Trader, symbol, date
         
-//        select top 300 * from trade where date = '2019-01-03' and symbol = 'SBUX';
+        
+//        select count(*) from trade;
+//        select top 300 * from trade where date >= '2018-01-03' and symbol = 'SBUX' order by date;
+//        select * from trade order by date, symbol;
+
 
         -----------------------------------------------------
           create or replace view finserv.public.position comment = 'what assets owned; demo Window Function running sum'
@@ -139,20 +239,10 @@ alter warehouse finserv_devops_wh set warehouse_size = 'medium';
             (num_shares_cumulative * close) + cash_cumulative as PnL
           from cte;
           
-          select top 300 * 
-          from position where date between '2019-01-01' and  '2019-01-31' and symbol = 'SBUX' and trader = 'charles'
-          order by date;
+//          select top 300 * 
+//          from position where date between '2019-01-01' and  '2019-01-31' and symbol = 'SBUX' and trader = 'charles'
+//          order by date;
           
-          
-          select 
-              t.symbol, exchange, t.date, trader, pm,
-              Sum(num_shares) OVER(partition BY t.symbol, exchange, trader ORDER BY t.date rows UNBOUNDED PRECEDING ) num_shares_cumulative,
-              Sum(cash) OVER(partition BY t.symbol, exchange, trader ORDER BY t.date rows UNBOUNDED PRECEDING ) cash_cumulative
-                ,s.close
-          from finserv.public.trade t
-          inner join finserv.public.stock_history s on t.symbol = s.symbol and s.date = t.date
-            where t.symbol = 'SBUX' and t.date > '2018-01-01' and trader = 'charles'
-                                                                                    order by 1,2,3;
 
 //select top 300 * from stock_history;
 
@@ -188,3 +278,6 @@ alter warehouse finserv_devops_wh set warehouse_size = 'medium';
         left outer join stock_latest l on p.symbol = l.symbol;
         
 //                select top 300 * from position_now where symbol = 'SBUX';
+
+        --size down to save money
+        alter warehouse finserv_devops_wh set warehouse_size = 'small';
