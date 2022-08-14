6c1403a134743acbd47830beb3478a40a68be0d5
Allen Wong <73718073+sfc-gh-awong@users.noreply.github.com>
2022-08-14 17:51:36 -0400

    Update finserv hol admin 10.sql

--- hands-on-lab/finserv hol admin 10.sql
+++ hands-on-lab/finserv hol admin 10.sql
@@ -28,77 +28,65 @@ https://github.com/Snowflake-Labs/sfguide-financial-asset-management/tree/master
 
     //Create compute
     create warehouse if not exists fs_hol_xsmall with warehouse_size = 'xsmall' auto_suspend = 60 initially_suspended = true max_cluster_count = 6;
-    create warehouse if not exists fs_hol_xlarge with warehouse_size = 'xlarge' auto_suspend = 60 initially_suspended = true max_cluster_count = 4;
+//    create warehouse if not exists fs_hol_xlarge with warehouse_size = 'xlarge' auto_suspend = 60 initially_suspended = true max_cluster_count = 4;
 
     grant ownership on warehouse fs_hol_xsmall to role finservam_admin;
-    grant ownership on warehouse fs_hol_xlarge to role finservam_admin;
+//    grant ownership on warehouse fs_hol_xlarge to role finservam_admin;
 
 
 ----------------------------------------------------------------------------------------------------------
 --Zero Copy Clone fs_hol1 only
-create or replace database fs_hol1 clone finservam;
 
+create database fs_hol1 clone finservam;
+create database fs_hol2 clone finservam;
+create database fs_hol3 clone finservam;
+create database fs_hol4 clone finservam;
+create database fs_hol5 clone finservam;
 
 
 
 -----------------------------------------------------
---grant
-
-
-
-    -----------------------------------------------------
-    --grand read on prod
-//    grant usage on database 
-
-
-    -----------------------------------------------------
-    --grant ownership on 
-//    grant ownership on database fs_hol1 to role fs_hol_rl;
-    grant all privileges on database fs_hol1 to role fs_hol_rl;
-
-    grant ownership on schema fs_hol1.public to role fs_hol_rl;
-    grant ownership on schema fs_hol1.middleware to role fs_hol_rl;
-    
-    grant ownership on all tables in schema fs_hol1.public to role fs_hol_rl copy current grants;
-    grant ownership on all views in schema fs_hol1.public to role fs_hol_rl copy current grants;
-    grant ownership on all views in schema fs_hol1.middleware to role fs_hol_rl copy current grants;
-
-    grant monitor, operate, usage on warehouse fs_hol_xsmall to role fs_hol_rl;
-    
-
------------------------------------------------------
---create user
-//    drop user if exists fs_hol_user1;
-
-create user fs_hol_user1 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl1;
-create user fs_hol_user2 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl2;
-
-
+--role
 create role fs_hol_rl1; grant role fs_hol_rl1 to role finservam_admin;
 create role fs_hol_rl2; grant role fs_hol_rl2 to role finservam_admin;
+create role fs_hol_rl3; grant role fs_hol_rl3 to role finservam_admin;
+create role fs_hol_rl4; grant role fs_hol_rl4 to role finservam_admin;
+create role fs_hol_rl5; grant role fs_hol_rl5 to role finservam_admin;
 
+
+
+-----------------------------------------------------
+--user
+create user fs_hol_user1 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl1;
+create user fs_hol_user2 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl2;
+create user fs_hol_user3 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl3;
+create user fs_hol_user4 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl4;
+create user fs_hol_user5 password = 'replaceme', default_warehouse = fs_hol_xsmall, default_namespace = fs_hol_prod.public, default_role = fs_hol_rl5;
+
+-----------------------------------------------------
+--grant role to user
 grant role fs_hol_rl1 to user fs_hol_user1;
-grant role fs_hol_rl2 to user fs_hol_user2; grant role fs_hol_rl to user fs_hol_user2;
+grant role fs_hol_rl2 to user fs_hol_user2;
+grant role fs_hol_rl3 to user fs_hol_user3;
+grant role fs_hol_rl4 to user fs_hol_user4;
+grant role fs_hol_rl5 to user fs_hol_user5;
 
-show grants to role fs_hol_rl;
-show grants to user fs_hol_user2;
 
------------------------------------------------------
---
-create database fs_hol2 clone fs_hol1;
-grant usage on database fs_hol2 to role fs_hol_rl;
-
+//show grants to role fs_hol_rl;
+//show grants to user fs_hol_user2;
 
 -----------------------------------------------------
 --
+//create database fs_hol2 clone fs_hol1;
+//grant usage on database fs_hol2 to role fs_hol_rl;
 
-    grant all privileges on database fs_hol2 to role fs_hol_rl2;
 
-    grant ownership on schema fs_hol2.public to role fs_hol_rl2;
-    grant ownership on schema fs_hol2.middleware to role fs_hol_rl2;
-    
-    grant ownership on all tables in schema fs_hol2.public to role fs_hol_rl2 copy current grants;
-    grant ownership on all views in schema fs_hol2.public to role fs_hol_rl2 copy current grants;
-    grant ownership on all views in schema fs_hol2.middleware to role fs_hol_rl2 copy current grants;
+-----------------------------------------------------
+--
 
-    grant monitor, operate, usage on warehouse fs_hol_xsmall to role fs_hol_rl2;
+-----------------------------------------------------
+--
+
+grant all privileges on database fs_hol2 to role fs_hol_rl2; grant ownership on schema fs_hol2.public to role fs_hol_rl2; grant ownership on schema fs_hol2.middleware to role fs_hol_rl2; grant ownership on all tables in schema fs_hol2.public to role fs_hol_rl2; grant ownership on all views in schema fs_hol2.public to role fs_hol_rl2; grant ownership on all views in schema fs_hol2.middleware to role fs_hol_rl2; grant monitor, operate, usage on warehouse fs_hol_xsmall to role fs_hol_rl2;
+
+grant all privileges on database fs_hol5 to role fs_hol_rl5; grant ownership on schema fs_hol5.public to role fs_hol_rl5; grant ownership on schema fs_hol5.middleware to role fs_hol_rl5; grant ownership on all tables in schema fs_hol5.public to role fs_hol_rl5; grant ownership on all views in schema fs_hol5.public to role fs_hol_rl5; grant ownership on all views in schema fs_hol5.middleware to role fs_hol_rl5; grant monitor, operate, usage on warehouse fs_hol_xsmall to role fs_hol_rl5;
