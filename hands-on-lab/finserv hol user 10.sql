ba2fab18d89cb5ef23b892c121bf9f7a3b758e80
Allen Wong <73718073+sfc-gh-awong@users.noreply.github.com>
2022-09-05 09:55:42 -0400

    added clone, update, delete, swap with

--- hands-on-lab/finserv hol user 10.sql
+++ hands-on-lab/finserv hol user 10.sql
@@ -6,7 +6,8 @@ INSTRUCTIONS
 1. Find & Replace in Snowsight or the text editor of your choice:
     for mac cmd-shift-h
     for windows ctrl-shift-h
-2. Replace all occurences 999 with the UserID you were assigned ie 1-30.
+2. Replace all occurences of fs_hol999 with "fs_hol" and the UserID you were assigned ie 1-30.
+3. Change the fs_hol_rl999 to your UserID ie fs_hol_rl" and your UserID 
 
 
 */
@@ -32,25 +33,6 @@ INSTRUCTIONS
     from fs_hol_prod.information_schema.columns
     where table_schema = 'PUBLIC' and comment is not null
     order by 1,2;
-
-
-
-
-    //what is the current PnL for trader charles? - view on trade table so always updated as trade populated
-        //notice it is a non-materialized window function view on 2 billion rows
-        select *
-        from fs_hol_prod.public.position_now where trader = 'charles'
-        order by PnL desc;
-        
-                
-        
-    //see ranked PnL for a random trader - no indexes, statistics, vacuuming, maintenance
-        set trader = (select top 1 trader from fs_hol_prod.public.trader sample(10) where trader is not null);
-
-        select symbol, date, trader, PM, cash_now, num_share_now, close, market_value, PnL
-        from fs_hol_prod.public.position_now
-        where trader = $trader
-        order by PnL desc;
         
         
 
@@ -64,13 +46,23 @@ INSTRUCTIONS
           //dynamic view using window functions so only pay storage for trade table; trade table drives all
               select get_ddl('view','fs_hol_prod.public.position');   
               
+          //how many rows is in the trade table that we just queried?
+            //let's see the metadata cache in the query profile
+              select count(*) from trade;
+              
 
+
+    //see ranked PnL for a random trader - no indexes, statistics, vacuuming, maintenance
+        set trader = (select top 1 trader from fs_hol_prod.public.trader sample(10) where trader is not null);
+
+        select symbol, date, trader, PM, cash_now, num_share_now, close, market_value, PnL
+        from fs_hol_prod.public.position_now
+        where trader = $trader
+        order by PnL desc;
 
 
 
     //trade - date and quantity of buy, sell, or hold action on assets: This controls the position view
-        select count(*) from fs_hol_prod.public.trade;
-        
         select * 
         from fs_hol_prod.public.trade 
         where date >= '2019-01-01' and symbol = 'AMZN' and trader = 'charles'
@@ -99,7 +91,10 @@ INSTRUCTIONS
         from zepl_us_stocks_daily.public.stock_history
         where symbol = 'SBUX'
         order by date desc;
-        
+
+        //Shares are read-only pointers - uncomment this command and watch it fail
+        -- delete from zepl_us_stocks_daily.public.stock_history;
+
 
 
 
@@ -109,12 +104,12 @@ INSTRUCTIONS
 ----------------------------------------------------------------------------------------------------------
 --Zero Copy Clone for instant dev,qa,sandboxes
 
-
+//this is prod
   select *
   from fs_hol_prod.public.trade 
   where trader = 'charles' and symbol = 'AMZN';
 
-//we want to change this
+//we want to change our test / clone
   select *
   from fs_hol999.public.trade 
   where trader = 'charles' and symbol = 'AMZN';
@@ -137,7 +132,7 @@ INSTRUCTIONS
   
   
                   //we can also see and set the queryID via: Home | Activity | Query History
-                      //set queryID = '01a61bc3-0504-9531-0000-7335021bd556';
+                      //set queryID = 'changeMe';
 
 
 
@@ -150,7 +145,7 @@ INSTRUCTIONS
   
   -----------------------------------------------------
   --UNDO our delete
-  insert into fs_hol1.public.trade 
+  insert into fs_hol999.public.trade 
   select *
   from fs_hol999.public.trade 
   before (statement => $queryid)
@@ -165,15 +160,66 @@ INSTRUCTIONS
           from fs_hol999.public.trade 
           where trader = 'charles' and symbol = 'AMZN';
 
+//we can also clone
+  create transient table fs_hol999.public.trade_clone clone fs_hol999.public.trade;
+  
+  //let's test updates against our clone
+    select *
+    from fs_hol999.public.trade_clone
+    where trader = 'charles' and symbol = 'AMZN';
 
---Uncomment this section to test undrop table
+    update fs_hol999.public.trade_clone set num_shares = num_shares * 10, cash = cash * 10
+    where trader = 'charles' and symbol = 'AMZN';
+    
+    set queryID_update = last_query_id(); 
+    
+    select *
+    from fs_hol999.public.trade 
+    before (statement => $queryID_update)
+    where trader = 'charles' and symbol = 'AMZN';
+
+  //let's test deletes against our clone
+    select *
+    from fs_hol999.public.trade_clone
+    where trader = 'charles' and symbol = 'TSLA';
+
+    delete from fs_hol999.public.trade_clone
+    where trader = 'charles' and symbol = 'TSLA';
+    
+    set queryID_delete = last_query_id(); 
+    
+    select *
+    from fs_hol999.public.trade 
+    before (statement => $queryID_delete)
+    where trader = 'charles' and symbol = 'TSLA';
+    
+  //let's swap our UAT table with our clone then rename the previous UAT
+    alter table fs_hol999.public.trade swap with fs_hol999.public.trade_clone;
+    
+    //verify
+      alter table fs_hol999.public.trade_clone rename to fs_hol999.public.trade_previous;
+  
+      //TSLA is gone  
+      select * 
+      from fs_hol999.public.position_now 
+      where trader = 'charles'
+      order by pnl desc;
+
+      //AMZN has been recalculated
+      select 'prod' env, * from fs_hol_prod.public.position_now where trader = 'charles' and symbol = 'AMZN'
+          union all
+      select 'uat' env,  * from fs_hol999.public.position_now where trader = 'charles' and symbol = 'AMZN';
+
+
 /*
+--Uncomment this section to test undrop table
+
   -----------------------------------------------------
   --Undrop is also up to 90 days of Time Travel; DBAs and Release Managers sleep much better than backup & restore
   drop table fs_hol999.public.trade;
 
   //this will fail until you undrop the table
-  select count(*) from fs_hol1.public.trade;
+  select count(*) from fs_hol999.public.trade;
   undrop table fs_hol999.public.trade;
   
   //verify undrop
