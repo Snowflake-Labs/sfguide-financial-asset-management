9aebd68f1bd71d2ed5a9ac1b78a28636c0c9ccbd
Allen Wong <53224894+allen-wong-tech@users.noreply.github.com>
2023-03-26 14:38:51 -0400

    Update finserv demo 20 Marketplace.sql

--- setup/finserv demo 20 Marketplace.sql
+++ setup/finserv demo 20 Marketplace.sql
@@ -1,7 +1,11 @@
 /*
 Summary:
-    Data Governance via RBAC by adding finservam_admin as role that can access data
-    Data Quality Check by ensuring no duplicates
+    verify economy_data_atlas share
+    remove duplicates
+
+    Python Faker function
+    https://medium.com/snowflake/flaker-2-0-fake-snowflake-data-the-easy-way-dc5e65225a13
+
 
 */
 
@@ -15,33 +19,89 @@ Summary:
     select top 1 *
     from economy_data_atlas.economy.usindssp2020;
 
+    
+--exclude_symbol - we will exclude these as they have the same symbol in NASDAQ and NYSE
+    create or replace table transform.exclude_symbol as
+    select distinct "Company" symbol
+    from
+    (
+        select "Company", "Date"
+        from economy_data_atlas.economy.usindssp2020
+        where "Indicator Name" = 'Close'
+        and "Scale" = 1
+        and "Frequency" = 'D'
+        and "Stock Exchange Name" in ('NASDAQ','NYSE')
+        group by "Company", "Date"
+        having count(*) > 1
+    )
+    order by 1;
 
---Verify snowflake_sample_data share
-    select top 1 *
-    from snowflake_sample_data.tpcds_sf10tcl.customer;
+--close price = 0 we will exclude
+    insert into transform.exclude_symbol (symbol)
+    with cte as
+    (
+        select distinct "Company" symbol
+        from economy_data_atlas.economy.usindssp2020
+        where "Indicator Name" = 'Close'
+        and "Scale" = 1
+        and "Frequency" = 'D'
+        and "Stock Exchange Name" in ('NASDAQ','NYSE')
+        and "Value" = 0
+    )
+    select c.symbol
+    from cte c
+    left outer join transform.exclude_symbol q on c.symbol = q.symbol 
+    where q.symbol is null
+    order by 1;
 
 
 ----------------------------------------------------------------------------------------------------------
 --stock_history
+    create or replace transient table finservam.public.stock_history
+        comment = 'knoema economy_data_atlas.economy.usindssp2020 daily closing prices for NASDAQ & NYSE'
+    as
+    select
+        "Company" symbol,
+        "Date" date,
+        "Company Name" company,
+        "Stock Exchange Name" exchange,
+        "Value" close
+    from economy_data_atlas.economy.usindssp2020 k
+    left outer join transform.exclude_symbol e on e.symbol = k."Company"
+    where "Indicator Name" = 'Close'
+    and "Scale" = 1
+    and "Frequency" = 'D'
+    and "Stock Exchange Name" in ('NASDAQ','NYSE')
+    and e.symbol is null
+    and "Company" not in ('FERG','BRKa')    --these are greater than $5000 per sharp
+    order by "Company", "Date";
+    
+    comment on column stock_history.close is 'security price at the end of the financial market business day';
 
-create or replace transient table finservam.public.stock_history
-    comment = 'knoema economy_data_atlas.economy.usindssp2020 daily closing prices for NASDAQ & NYSE'
-as
-select
-    "Company" symbol,
-    "Date" date,
-    "Company Name" company,
-    "Stock Exchange Name" exchange,
-    "Value" close
-from economy_data_atlas.economy.usindssp2020
-where "Indicator Name" = 'Close'
-and "Scale" = 1
-and "Frequency" = 'D'
-and "Stock Exchange Name" in ('NASDAQ','NYSE')
-order by "Company", "Date";
 
-comment on column stock_history.close is 'security price at the end of the financial market business day';
+-----------------------------------------------------
+--Python FAKE
+    create or replace function FAKE(locale varchar,provider varchar,parameters variant)
+    returns variant
+    language python
+    volatile
+    runtime_version = '3.8'
+    packages = ('faker','simplejson')
+    handler = 'fake'
+    as
+    $$
+    import simplejson as json
+    from faker import Faker
+    def fake(locale,provider,parameters):
+      if type(parameters).__name__=='sqlNullWrapper':
+        parameters = {}
+      fake = Faker(locale=locale)
+      return json.loads(json.dumps(fake.format(formatter=provider,**parameters), default=str))
+    $$;
 
+/*
+--test
+select FAKE('en_US','name',null)::varchar as FAKE_NAME
+from table(generator(rowcount => 10));
 
---optional: if we don't want to wait for auto-suspend:
-    alter warehouse finservam_devops_wh suspend;
+*/
